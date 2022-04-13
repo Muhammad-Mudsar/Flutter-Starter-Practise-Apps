989574a691597bf4e81287998e27b56f265a9fa4
Muhammad MUDSAR <muhammadmudsarch@gmail.com>
2022-04-13 22:36:51 +0500

    gesture Detector
    gesture detector functionality added

--- Bmi_app/bmi_app_prectise/lib/input_page.dart
+++ Bmi_app/bmi_app_prectise/lib/input_page.dart
@@ -3,69 +3,104 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'iconTextfile.dart';
 import 'containerfile.dart';
 
+const activecolor=Color(0xFF1D1E33);
+const deactivecolor=Color(0xFF111328);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:Text('BMI CALCULATOR'),
-      ),
-      body: Column(
-        children:<Widget>[
-          Expanded(child:Row(
-            children: <Widget> [
-            Expanded(
-              child: RepeatContainerCodemi(
-                colors:Color(0xFF0A0E21),
-                CardWidget:RepeatTextandIconWidget(
-                  iconData:FontAwesomeIcons.male,
-                  label: 'Male',
+
+  Color malecolor = deactivecolor;
+  Color femalecolor = deactivecolor;
+
+  void updateColor(int gender) {
+    if (gender == 1) {
+      Color malecolor = activecolor;
+      Color femalecolor = deactivecolor;
+    }
+    if (gender == 2) {
+      Color malecolor = deactivecolor;
+      Color femalecolor = activecolor;
+    }
+  }
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        updateColor(1);
+                      });
+                    },
+                    child: RepeatContainerCodemi(
+                      //alt+enter
+                      colors: malecolor,
+                      CardWidget: RepeatTextandIconWidget(
+                        iconData: FontAwesomeIcons.male,
+                        label: 'Male',
+                      ),
+                    ),
+                  ),
+
                 ),
-              ),
+                Expanded(child: GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      updateColor(2);
+                    });
+                  },
+                  child: RepeatContainerCodemi(
+                    colors: femalecolor,
+                    CardWidget: RepeatTextandIconWidget(
+                      iconData: FontAwesomeIcons.female,
+                      label: 'Female',
 
+                    ),
+
+                  ),
+                ),
+                ),
+
+
+              ],
+            ),),
+            Expanded(child: RepeatContainerCodemi(colors: Color(0xFF0A0E21),),
             ),
-            Expanded(child: RepeatContainerCodemi(
-              colors:Color(0xFF0A0E21),
-              CardWidget:RepeatTextandIconWidget(
-                iconData:FontAwesomeIcons.female,
-                label:'Female',
-
-              ),
-
-            ),
-            ),
+            Expanded(child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: RepeatContainerCodemi(colors: Color(0xFF0A0E21),)
+                  ,),
+                Expanded(
+                  child: RepeatContainerCodemi(colors: Color(0xFF0A0E21),),
+                ),
 
 
-    ],
-      ),),
-          Expanded(child:RepeatContainerCodemi(colors:Color(0xFF0A0E21),),
-          ),
-          Expanded(child:Row(
-          children: <Widget> [
-          Expanded(child:  RepeatContainerCodemi(colors:Color(0xFF0A0E21),)
-            ,),
-          Expanded(child: RepeatContainerCodemi(colors:Color(0xFF0A0E21),),
-          ),
+              ],
+            ),),
 
-
-        ],
-      ),),
-
-        ],
-      ),
+          ],
+        ),
       );
-      floatingActionButton: FloatingActionButton(
-        onPressed:(){},
+      floatingActionButton:
+      FloatingActionButton(
+        onPressed: () {},
         child: Icon(Icons.add),
 
-        );
+      );
+    }
   }
-}
 
 
 
